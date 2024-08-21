4c8d4e1df2e9f8258fed785e616fefeab9609ae4
Lucas <lucas.iferreir@gmail.com>
2024-08-21 09:19:35 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Lucas");
+        curso1.setTitulo("Java");
+        curso1.setCargaHoraria(7);
+
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Lucas");
+        curso2.setTitulo("JavaScript");
+        curso2.setCargaHoraria(8);
     }
 }
